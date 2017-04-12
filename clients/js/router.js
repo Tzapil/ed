50b13899bd099fd7aebe9687888fb902125bce71
du.kulaevskiy <du.kulaevskiy@tensor.ru>
2017-04-12 17:03:14 +0300

    routing

--- clients/js/router.js
+++ clients/js/router.js
@@ -1,16 +1,13 @@
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl : "index.html"
-    })
-    .when("/red", {
-        templateUrl : "red.htm"
-    })
-    .when("/green", {
-        templateUrl : "green.htm"
-    })
-    .when("/blue", {
-        templateUrl : "blue.htm"
-    });
+      .when("/home", {
+          templateUrl : "welcome.htm"
+      })
+      .when("/users", {
+          templateUrl : "users.htm"
+      })
+      .otherwise({
+        redirectTo: "/home"
+      });
 });
--- clients/js/script.js
+++ clients/js/script.js
@@ -1,3 +1,3 @@
 'use strict';
 
-var app = angular.module('testApp', []);
+var app = angular.module('myApp', []);
