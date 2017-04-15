8ccddd6cd42ecc5d70da76a827762d9aeca3e556
Tzapil <thhwitcher@gmail.com>
2017-04-15 16:17:12 +0300

    pagination

--- clients/js/controllers.js
+++ clients/js/controllers.js
@@ -15,7 +15,7 @@ app
     }
   }])
   .controller('TabsController', ['$scope', function ($scope) {
-    $scope.tab = 3;
+    $scope.tab = 1;
 
     $scope.setTab = function(newTab){
       $scope.tab = newTab;
@@ -26,20 +26,9 @@ app
     };
   }])
   .controller('UserController', ['$scope', '$http', function ($scope, $http) {
-      $scope.headers = ["Name", "Email", "Department", "SSN"];
+      $scope.headers = ["Name", "Email", "Department", "Telephone"];
       $scope.users = [];
-      $scope.propertyName = 'name';
-      $scope.reverse = false;
       $scope.currentView = 'page1';
-
-      $scope.sortBy = function(propertyName) {
-        $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
-        $scope.propertyName = propertyName;
-      };
-
-      $scope.isSorted = function(property) {
-        return $scope.propertyName == property;
-      };
 
       $http.get('/data').then(function (answer) {
         $scope.users = answer.data;
@@ -48,6 +37,7 @@ app
         for(var i = 0; i < $scope.users.length; i++) {
            var item = $scope.users[i];
            item.name = item.first_name + ' ' + item.last_name;
+           item.telephone = "+123(456)123-45-76";
         }
 
         // build groups
@@ -65,4 +55,79 @@ app
            temp[item.group].push(item);
         }
       });
+  }])
+  .controller('ListController', ['$scope', '$filter', function ($scope, $filter) {
+    // Sort
+    $scope.sort = {
+      propertyName : 'name',
+      reverse : false
+    };
+
+    $scope.sortBy = function(propertyName) {
+      $scope.sort.reverse = ($scope.sort.propertyName === propertyName) ? !$scope.sort.reverse : false;
+      $scope.sort.propertyName = propertyName;
+
+      $scope.search();
+    };
+
+    $scope.isSorted = function(property) {
+      return $scope.sort.propertyName == property;
+    };
+
+    // Pagination
+    $scope.gap = 5;
+
+    $scope.filteredItems = [];
+    $scope.itemsPerPage = 10;
+    $scope.pagedItems = [];
+
+    $scope.currentPage = 1;
+
+    $scope.$watch('searchName', function (newVal, oldVal) {
+  		$scope.currentPage = 1;
+
+      $scope.search();
+  	}, true);
+
+    $scope.search = function () {
+      $scope.filteredItems = $filter('filter')($scope.users, {name: $scope.searchName});
+      // take care of the sorting order
+      if ($scope.sort.propertyName !== '') {
+        $scope.filteredItems = $filter('orderBy')($scope.filteredItems, $scope.sort.propertyName, $scope.sort.reverse);
+      }
+      // now group by pages`
+      $scope.groupToPages();
+    };
+
+    // calculate page in place
+    $scope.groupToPages = function () {
+      $scope.pagedItems = [];
+
+      for (var i = 0; i < $scope.filteredItems.length; i++) {
+        if (i % $scope.itemsPerPage === 0) {
+          $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [ $scope.filteredItems[i] ];
+        } else {
+          $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.filteredItems[i]);
+        }
+      }
+    };
+
+    $scope.range = function (size, start, end) {
+      var result = [];
+
+      if (size < end) {
+        end = size;
+        start = size - $scope.gap;
+      }
+      for (var i = start; i < end; i++) {
+        result.push(i);
+      }
+      return result;
+    };
+
+    $scope.setPage = function (page) {
+      $scope.currentPage = page;
+    };
+
+    $scope.search();
   }]);
