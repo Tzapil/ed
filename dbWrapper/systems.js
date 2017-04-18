086013aca5d55310a5b6ff09f05163df97695634
du.kulaevskiy <du.kulaevskiy@tensor.ru>
2017-04-18 18:55:05 +0300

    work1

--- dbWrapper/systems.js
+++ dbWrapper/systems.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
-// id,edsm_id,name,x,y,z,population,is_populated,government_id,government,allegiance_id,allegiance,state_id,state,security_id,security,primary_economy_id,primary_economy,power,power_state,power_state_id,needs_permit,updated_at,simbad_ref,controlling_minor_faction_id,controlling_minor_faction,reserve_type_id,reserve_type
+// id,edsm_id,name,x,y,z,population,is_populated,government_id,government,allegiance_id,allegiance,state_id,state,security_id,security,primary_economy_id,primary_economy
+// power,power_state,power_state_id,needs_permit,updated_at,simbad_ref,controlling_minor_faction_id,controlling_minor_faction,reserve_type_id,reserve_type
 
 var Schema = mongoose.Schema;
 var System = new Schema({
@@ -8,10 +9,15 @@ var System = new Schema({
     type: Number,
     unique: true
   },
+  edsm_id: Number,
   name: {
-    type: String,
-    unique: true
+    type: String
   },
+  x: Number,
+  y: Number,
+  z: Number,
+  population: String,
+  is_populated: Boolean,
   updated_at: Number,
   government_id: Number,
   government: String,
@@ -19,10 +25,25 @@ var System = new Schema({
   allegiance: String,
   state_id: Number,
   state: String,
+  security_id: Number,
+  security: String,
+  primary_economy_id: Number,
+  primary_economy: Number,
+  needs_permit: Boolean,
   home_system_id: Number,
   is_player_faction: Boolean
 });
 
+// as == another system
+System.methods.distance = function (as) {
+  var dx = (this.x - as.x);
+  var dy = (this.y - as.y);
+  var dz = (this.z - as.z);
+
+  return Math.sqrt(dx + dy + dz)
+}
+
 var SystemModel = mongoose.model('System', System);
 
 module.exports.SystemModel = SystemModel;
+module.exports.url = "https://eddb.io/archive/v5/systems_populated.json";
--- index.js
+++ index.js
@@ -5,23 +5,120 @@ var config = require('./config');
 var helpers = require('./helpers');
 var commodity = require('./dbWrapper/commodity');
 var factions = require('./dbWrapper/factions');
-var systems_recently = require('./dbWrapper/systems_recently');
+var systems = require('./dbWrapper/systems');
 
 mongoose.connect(config.mongo, config.options);
 var db = mongoose.connection;
+
+function any(arr, predicate) {
+  var result = false;
+
+  for (var i = 0; i < arr.length; i++) {
+    if (predicate(arr[i])) {
+      result = true;
+      break;
+    }
+  }
+
+  return result;
+}
+
+function findSystemsInRange(system, range, exclude) {
+  exclude = exclude || [];
+
+  return new Promise(function (resolve, reject) {
+    systems.SystemModel.find({id: {$ne: system.id},
+                              x: {$gte: system.x - range, $lte: system.x + range},
+                              y: {$gte: system.y - range, $lte: system.y + range},
+                              z: {$gte: system.z - range, $lte: system.z + range}}, function (err, systems) {
+      if (err) reject(err);
+
+      resolve(systems.filter((item) => item.distance(system) <= range && !any(exclude, (e_item) => e_item == item.id)));
+    });
+  });
+}
+
+function sortSystemsThoughRange(system) {
+  return function (systems) {
+    systems = systems || [];
+
+    return new Promise(function (resolve, reject) {
+      systems.sort(function (a, b) {
+        var as = a.distance(system);
+        var bs = b.distance(system);
+        if (as < bs) {
+          return -1;
+        }
+
+        if (as > bs) {
+          return 1;
+        }
+
+        return 0
+      });
+
+      resolve(systems);
+    });
+  }
+}
+
+function findPath (s1, s2, fsd, path) {
+  path = path || [];
+
+  return new Promise(function (resolve, reject) {
+    findSystemsInRange(s1, fsd, path)
+      .then(sortSystemsThoughRange(s2))
+      .then(function (systems) {
+        if (systems.length == 0) {
+          resolve([]);
+          return;
+        }
+
+        if (systems[0].id == s2.id) {
+          resolve([s1, s2]);
+        }
+
+        return findPath(systems[0], s2, [s1]);
+      });
+  });
+}
+
 
 db.on("open", function () {
   console.log("db connected");
 
   var start = new Date();
   //helpers.updateDB(commodity.CommodityModel, commodity.url);
-  helpers.updateDB(factions.FactionModel, factions.url).then(function () {
-    console.log("done", (new Date()).getTime() - start.getTime(), "ms");
-    process.exit(1);
-  }, function () {
-    console.log("error");
-    process.exit(1);
-  });
+  // helpers.updateDB(systems.SystemModel, systems.url).then(function () {
+  //   console.log("done", (new Date()).getTime() - start.getTime(), "ms");
+  //   process.exit(1);
+  // }, function () {
+  //   console.log("error");
+  //   process.exit(1);
+  // });
+
+  console.log("Start to search");
+  systems.SystemModel.findOne({name: "Sol"}, function (err, system) {
+    if (err) throw err;
+
+    var fsd_power = 18;
+
+    findSystemsInRange(system, fsd_power).then(sortSystemsThoughRange(system)).then(function (systems) {
+      console.log(systems.length);
+      console.log(systems.map((item) => {
+        return {
+          name: item.name,
+          r: item.distance(system)
+        }
+      }));
+      console.log("done", (new Date()).getTime() - start.getTime(), "ms");
+      process.exit(1);
+    })
+    .catch(function () {
+      console.log("error", (new Date()).getTime() - start.getTime(), "ms");
+      process.exit(1);
+    });
+  })
   // factions.updateDB();
   //systems_recently.updateDB();
 });
